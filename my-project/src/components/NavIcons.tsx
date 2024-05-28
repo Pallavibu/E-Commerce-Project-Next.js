97be6f5dd193c661a1a1179ca96d77df8ef3a1f6
Pallavibu <pallavibu44@gmail.com>
2024-05-29 02:47:44 +0530

    working on search button

--- my-project/src/components/NavIcons.tsx
+++ my-project/src/components/NavIcons.tsx
@@ -1,8 +1,29 @@
 'use client'
+import Image from "next/image"
+import { useState } from "react"
+import Link from "next/link"
 
 const NavIcons = () => {
+
+const[isProfileOpen, setIsProfileOpen] = useState(false)
+
+const[isCartOpen, setIsCartOpen] = useState(false)
+
     return (
-        <div className="">Navicons</div>
+        <div className="flex items-center gap-4 xl:gap-6">
+         <Image src="/profile.png" alt="" height={22} width={22} className="cursor-pointer"/>
+
+         {
+            isProfileOpen && <div className="">
+                <Link href="">Profile</Link>
+            </div>
+         }
+
+         <Image src="/notification.png" alt="" height={22} width={22} className="cursor-pointer"/>
+
+         <Image src="/cart.png" alt="" height={22} width={22} className="cursor-pointer"/>
+
+        </div>
     )
 }
 
