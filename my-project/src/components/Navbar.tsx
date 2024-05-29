4016d4889f70b3572462a883aac02cb8a3d144cf
Pallavibu <pallavibu44@gmail.com>
2024-05-30 01:23:13 +0530

    working on nav icons

--- my-project/src/components/Navbar.tsx
+++ my-project/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Menu from "./Menu";
 import Image from "next/image";
-import SearchBar from "./SearchBar"
+import SearchBar from "./SearchBar";
 import NavIcons from "./NavIcons";
 
 const Navbar = () => {
@@ -17,16 +17,23 @@ const Navbar = () => {
       {/*BIGGER SCREENS*/}
       <div className="hidden md:flex items-center justify-between h-full gap-8">
         {/*LEFT*/}
-        <div className="w-1/3">
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
           <Link href="/" className="flex items-center gap-3">
-            <Image src='/logo.png' alt='' width={24} height={24}/>
+            <Image src="/logo.png" alt="" width={24} height={24} />
             <div className="text-2xl tracking-wide">PALLAVI</div>
           </Link>
+          <div className="hidden xl:flex gap-4">
+            <Link href="/">Homepage</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Deals</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </div>
         </div>
         {/*RIGHT*/}
-        <div className="w-2/3 flex items-center justify-between gap-8">
-         <SearchBar/>
-         <NavIcons/>
+        <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
+          <SearchBar />
+          <NavIcons />
         </div>
       </div>
     </div>
