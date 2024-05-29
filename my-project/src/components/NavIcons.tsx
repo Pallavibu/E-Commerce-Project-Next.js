620813a49f445a79ad1f382a5c654d5f94e3ac07
Pallavibu <pallavibu44@gmail.com>
2024-05-29 23:15:33 +0530

    working on nav bar

--- my-project/src/components/NavIcons.tsx
+++ my-project/src/components/NavIcons.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image"
 import { useState } from "react"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 const NavIcons = () => {
 
@@ -9,13 +10,24 @@ const[isProfileOpen, setIsProfileOpen] = useState(false)
 
 const[isCartOpen, setIsCartOpen] = useState(false)
 
+const router = useRouter()
+const isLoggedIn = false;
+
+const handleProfile = () => {
+if (!isLoggedIn) {
+    
+}
+}
+
     return (
         <div className="flex items-center gap-4 xl:gap-6">
-         <Image src="/profile.png" alt="" height={22} width={22} className="cursor-pointer"/>
+         <Image src="/profile.png" alt="" height={22} width={22} className="cursor-pointer"
+         onClick={handleProfile}/>
 
          {
             isProfileOpen && <div className="">
-                <Link href="">Profile</Link>
+                <Link href="/">Profile</Link>
+                <div className="mt-2 cursor-pointer">Logout</div>
             </div>
          }
 
