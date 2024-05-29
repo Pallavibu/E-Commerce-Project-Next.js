52df91f278517767ed382ab0c16b2626ca055af7
Pallavibu <pallavibu44@gmail.com>
2024-05-29 23:58:17 +0530

    working on nav bar

--- my-project/src/components/NavIcons.tsx
+++ my-project/src/components/NavIcons.tsx
@@ -1,42 +1,59 @@
-'use client'
-import Image from "next/image"
-import { useState } from "react"
-import Link from "next/link"
-import { useRouter } from "next/navigation"
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const NavIcons = () => {
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
 
-const[isProfileOpen, setIsProfileOpen] = useState(false)
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
-const[isCartOpen, setIsCartOpen] = useState(false)
+  const router = useRouter();
+  const isLoggedIn = false;
 
-const router = useRouter()
-const isLoggedIn = false;
+  const handleProfile = () => {
+    if (!isLoggedIn) {
+      router.push("/login");
+    }
+    setIsProfileOpen((prev) => !prev);
+  };
 
-const handleProfile = () => {
-if (!isLoggedIn) {
-    
-}
-}
+  return (
+    <div className="flex items-center gap-4 xl:gap-6 relative">
+      <Image
+        src="/profile.png"
+        alt=""
+        height={22}
+        width={22}
+        className="cursor-pointer"
+        onClick={handleProfile}
+      />
 
-    return (
-        <div className="flex items-center gap-4 xl:gap-6">
-         <Image src="/profile.png" alt="" height={22} width={22} className="cursor-pointer"
-         onClick={handleProfile}/>
-
-         {
-            isProfileOpen && <div className="">
-                <Link href="/">Profile</Link>
-                <div className="mt-2 cursor-pointer">Logout</div>
-            </div>
-         }
-
-         <Image src="/notification.png" alt="" height={22} width={22} className="cursor-pointer"/>
-
-         <Image src="/cart.png" alt="" height={22} width={22} className="cursor-pointer"/>
-
+      {isProfileOpen && (
+        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+          <Link href="/">Profile</Link>
+          <div className="mt-2 cursor-pointer">Logout</div>
         </div>
-    )
-}
+      )}
 
-export default NavIcons
+      <Image
+        src="/notification.png"
+        alt=""
+        height={22}
+        width={22}
+        className="cursor-pointer"
+      />
+
+      <Image
+        src="/cart.png"
+        alt=""
+        height={22}
+        width={22}
+        className="cursor-pointer"
+      />
+    </div>
+  );
+};
+
+export default NavIcons;
