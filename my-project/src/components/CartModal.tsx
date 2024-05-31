9a2734bcf6a79a0935a172eab43c4ce05b9ba540
Pallavibu <pallavibu44@gmail.com>
2024-05-31 22:08:11 +0530

    done with nav bar

--- my-project/src/components/CartModal.tsx
+++ my-project/src/components/CartModal.tsx
@@ -9,8 +9,9 @@ const CartModal = () => {
       {!cartItems ? (
         <div>Cart is empty</div>
       ) : (
-        //LIST
         <>
+          <h2 className="text-xl">Shopping Cart</h2>
+          {/* LIST*/}
           <div className="flex flex-col gap-8">
             {/*ITEM*/}
             <div className="flex gap-4">
@@ -67,11 +68,22 @@ const CartModal = () => {
               </div>
             </div>
           </div>
-              {/*BOTTOM*/}
+          {/*BOTTOM*/}
           <div className="">
-            <div>
-              <span></span>
-              <span></span>
+            <div className="flex items-center justify-between font-semibold">
+              <span>Subtotal</span>
+              <span>$49</span>
+            </div>
+            <p className="text-gray-500 text-sm mt-2 mb-4">
+              Shipping and taxes calculated at checkout
+            </p>
+            <div className="flex justify-between text-sm">
+              <button className="rounded-md ring-1 ring-gray-300 py-3 px-4">
+                View Cart
+              </button>
+              <button className="rounded-md py-3 px-4 bg-black text-white">
+                Checkout
+              </button>
             </div>
           </div>
         </>
