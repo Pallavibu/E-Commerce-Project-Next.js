f8aafe5e92d12eade6716db042d5213b6cf375b0
Pallavibu <pallavibu44@gmail.com>
2024-06-01 23:45:14 +0530

    working on slider

--- my-project/src/app/page.tsx
+++ my-project/src/app/page.tsx
@@ -1,6 +1,10 @@
+import Slider from "@/components/Slider";
+
 const HomePage = () => {
   return(
-    <div>HomePage</div>
+    <div>
+      <Slider/>
+    </div>
   )
 }
 
--- my-project/src/components/Slider.tsx
+++ my-project/src/components/Slider.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 const Slider = () => {
     return(
         <div>slider</div>
