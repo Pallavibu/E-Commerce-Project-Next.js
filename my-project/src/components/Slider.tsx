4b659a6baa6ea72fdf7e1923e1d8558336721c8c
Pallavibu <pallavibu44@gmail.com>
2024-06-01 23:56:09 +0530

    added images to slider

--- my-project/src/components/Slider.tsx
+++ my-project/src/components/Slider.tsx
@@ -1,5 +1,32 @@
 'use client'
 
+const slides = [
+    {
+        id : 1,
+        title:"Summer Sale Collection",
+        description:"Sale! Up to 50% off!",
+        img : "https://images.pexels.com/photos/25184945/pexels-photo-25184945/free-photo-of-fashion-eastern-dresses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        url : "/",
+        bg: "bg-gradient-to-r from-yellow-50 to-pink-50"
+    },
+    {
+        id : 2,
+        title:"Summer Sale Collection",
+        description:"Sale! Up to 50% off!",
+        img : "https://images.pexels.com/photos/25184945/pexels-photo-25184945/free-photo-of-fashion-eastern-dresses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        url : "/",
+        bg: "bg-gradient-to-r from-yellow-50 to-pink-50"
+    },
+    {
+        id : 3,
+        title:"Summer Sale Collection",
+        description:"Sale! Up to 50% off!",
+        img : "https://images.pexels.com/photos/25184945/pexels-photo-25184945/free-photo-of-fashion-eastern-dresses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        url : "/",
+        bg: "bg-gradient-to-r from-yellow-50 to-pink-50"
+    },
+]
+
 const Slider = () => {
     return(
         <div>slider</div>
