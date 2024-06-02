2349fe02019e69554ea45c28aa28cce01f406a7c
Pallavibu <pallavibu44@gmail.com>
2024-06-03 00:13:15 +0530

    working on slider

--- my-project/src/components/Slider.tsx
+++ my-project/src/components/Slider.tsx
@@ -43,11 +43,11 @@ const Slider = () => {
             key={slide.id}
           >
             {/*TEXT CONTAINER*/}
-            <div className="h-1/2 xl:w-1/2">
+            <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
               <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
               <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
               <Link href={slide.url}>
-                <button>SHOP NOW</button>
+                <button className="rounded-md bg-black text-white py-3 px-4">SHOP NOW</button>
               </Link>
             </div>
             {/*IMAGE CONTAINER*/}
@@ -63,6 +63,7 @@ const Slider = () => {
           </div>
         ))}
       </div>
+      
     </div>
   );
 };
