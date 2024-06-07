614a4bceee6a79672f39409ed61e07dcfeee6a2c
Pallavibu <pallavibu44@gmail.com>
2024-06-07 23:38:42 +0530

    working on single product details

--- my-project/src/components/ProductImages.tsx
+++ my-project/src/components/ProductImages.tsx
@@ -1,7 +1,32 @@
-const ProductImages = () => {
-    return(
-        <div></div>
-    )
-}
+import Image from "next/image";
 
-export default ProductImages
+const ProductImages = () => {
+  return (
+    <div>
+      <div className="h-[500px] relative">
+        <Image
+          src="https://images.pexels.com/photos/13008664/pexels-photo-13008664.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+          alt=""
+          fill
+          sizes="50vw"
+          className="object-cover rounded-md"
+        />
+      </div>
+      <div className="">
+        <div>
+          <Image
+            src="https://images.pexels.com/photos/13008664/pexels-photo-13008664.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            fill
+            sizes="50vw"
+            className="object-cover rounded-md"
+          />
+        </div>
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
