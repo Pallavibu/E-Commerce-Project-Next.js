47217a8c65584e2cd6fa549d1f17211581397ce8
Pallavibu <pallavibu44@gmail.com>
2024-06-10 20:31:36 +0530

    done with frontend now strting with backend

--- my-project/src/context/wixContext.tsx
+++ my-project/src/context/wixContext.tsx
@@ -1,18 +1,18 @@
 "use client"
 
 import { createClient, OAuthStrategy } from "@wix/sdk";
-
+import {products, collections} from "@wix/stores"
 
 
 const myWixClient = createClient({
     modules: {
       products,
-      services
+      //currentCart
     },
     auth: OAuthStrategy({
       clientId: '<YOUR_CLIENT_ID>',
       tokens: {
-        accessToken: {
+        accessToken: {  
           value: '<ACCESS_TOKEN_VALUE>',
           expiresAt: <ACCESS_TOKEN_EXPIRY_DATE>
 
