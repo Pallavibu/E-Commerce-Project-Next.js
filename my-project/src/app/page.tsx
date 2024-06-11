607cad8be3522b81300877ddfd7841549433d19d
Pallavibu <pallavibu44@gmail.com>
2024-06-12 01:04:34 +0530

    working with wixclient and wix server

--- my-project/src/app/page.tsx
+++ my-project/src/app/page.tsx
@@ -1,8 +1,24 @@
+"use client";
+
 import Slider from "@/components/Slider";
 import ProductList from "@/components/ProductList";
 import CategoryList from "@/components/CategoryList";
+import { useEffect } from "react";
+import { WixClientContext } from "@/context/wixContext";
+import { useWixClient } from "@/hooks/useWixClient";
 
 const HomePage = () => {
+  const wixClient = useWixClient()
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await wixClient.products.queryProducts().find();
+
+      console.log(res)
+    };
+
+    getProducts();
+  }, [wixClient]);
   return (
     <div>
       <Slider />
@@ -11,7 +27,9 @@ const HomePage = () => {
         <ProductList />
       </div>
       <div className="mt-24">
-        <h1 className="text-2xl px-4 md:px-8 lg-px-16 xl:px-32 2xl;px-64 mb-12">Categories</h1>
+        <h1 className="text-2xl px-4 md:px-8 lg-px-16 xl:px-32 2xl;px-64 mb-12">
+          Categories
+        </h1>
         <CategoryList />
       </div>
       <div className="mt-24  px-4 md:px-8 lg-px-16 xl:px-32 2xl;px-64">
