ea9095269ec973eefbdb12be2a6ea290e6774abd
Pallavibu <pallavibu44@gmail.com>
2024-06-12 22:39:12 +0530

    working with wixclient and wix server

--- my-project/src/app/page.tsx
+++ my-project/src/app/page.tsx
@@ -21,9 +21,9 @@ const HomePage = async () => {
   //     getProducts();
   //   }, [wixClient]);
 
-  //   const wixClient = await wixClientServer()
+  // const wixClient = await wixClientServer()
 
-  //   const res = await wixClient.products.queryProducts().find();
+  // const res = await wixClient.products.queryProducts().find();
 
   // console.log(res)
 
@@ -33,7 +33,10 @@ const HomePage = async () => {
       <div className="mt-24  px-4 md:px-8 lg-px-16 xl:px-32 2xl;px-64">
         <h1 className="text-2xl">Featured Products</h1>
         <Suspense fallback={"loading"}>
-        <ProductList categoryId = {process.env.FEATURED_PRODUCTS_CATEGORY_ID} limit={4}/>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+            limit={4}
+          />
         </Suspense>
       </div>
       <div className="mt-24">
